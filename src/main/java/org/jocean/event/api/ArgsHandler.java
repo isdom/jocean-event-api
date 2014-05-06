2bb9fe93f21d087b2bd1cd705d11cc44abf5e91b
ming.ma <ming.ma@upai.com>
2014-05-06 23:19:07 +0800

    在 ArgsHandler 接口定义中，增加 Consts内嵌类 下新增针对 idiom.ReferenceCounted 类型参数的 事件处理前后保护 ArgsHandler 实现: _REFCOUNTED_ARGS_GUARD

--- src/main/java/org/jocean/event/api/ArgsHandler.java
+++ src/main/java/org/jocean/event/api/ArgsHandler.java
@@ -3,6 +3,8 @@
  */
 package org.jocean.event.api;
 
+import org.jocean.idiom.ReferenceCounted;
+
 /**
  * @author isdom
  *
@@ -12,4 +14,31 @@ public interface ArgsHandler {
 	public Object[] beforeAcceptEvent(final Object[] args) throws Exception;
 	
 	public void afterAcceptEvent(final Object[] args) throws Exception;
+
+	final class Consts {
+        public static final ArgsHandler _REFCOUNTED_ARGS_GUARD = new ArgsHandler() {
+    
+            @Override
+            public Object[] beforeAcceptEvent(final Object[] args) {
+                if ( null != args ) {
+                    for ( Object arg : args) {
+                        if ( arg instanceof ReferenceCounted ) {
+                            ((ReferenceCounted<?>)arg).retain();
+                        }
+                    }
+                }
+                return args;
+            }
+    
+            @Override
+            public void afterAcceptEvent(final Object[] args) {
+                if ( null != args ) {
+                    for ( Object arg : args) {
+                        if ( arg instanceof ReferenceCounted ) {
+                            ((ReferenceCounted<?>)arg).release();
+                        }
+                    }
+                }
+            }};
+	}
 }
